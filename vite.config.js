90b04554baab6fb057ea538cbf6929b89a3b0048
Charvi <2320030371@klh.edu.in>
2025-04-18 00:59:10 +0530

    Fix vite base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// vite.config.js
 export default defineConfig({
+  // remove or comment out the base:
+  // base: '/some-path/',
   plugins: [react()],
-}) 
+});
